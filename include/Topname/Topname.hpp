fc253387f6efb9ae4d84cda8f269523acf01110a
Murat Toprak <murattoprak2004@gmail.com>
2024-08-29 19:23:49 +0300

    Implement a custom hash map and add a few utility functions - Add custom hash map which has an average time complexity of `O(1)` performs better than linear search with an average time complexity of `O(n)` when searching an enum value from a string - Add case insensitive string search mechanism - Add two methods to get all enums and strings in the `mappings` array

--- include/Topname/Topname.hpp
+++ include/Topname/Topname.hpp
@@ -20,32 +20,55 @@ constexpr auto enum_to_underlying(E enum_value) noexcept {
     return static_cast<std::underlying_type_t<E>>(enum_value);
 }
 
+// Hash function for better lookup performance
+constexpr uint32_t hash(std::string_view str) {
+    uint32_t hash = 5381;
+    for (char ch : str) {
+        hash = ((hash << 5) + hash) + ch; // hash * 33 + ch, but faster
+    }
+    return hash;
+}
+
+// Support case insensitive string comparison
+constexpr bool case_insensitive_equal(std::string_view a, std::string_view b) {
+    return std::ranges::equal(a, b, [](char c1, char c2) {
+        return std::tolower(static_cast<unsigned char>(c1)) ==
+               std::tolower(static_cast<unsigned char>(c2));
+    });
+}
+
 template<EnumType E, std::size_t N>
 class EnumString {
 public:
     template<typename... Args>
     constexpr EnumString(Args&&... args)
     : mappings{{std::forward<Args>(args)...}}
-    {}
+    {
+        build_hash_table();
+    }
 
-    [[nodiscard]] constexpr E to_enum_first(std::string_view value) const {
+    // Average time complexity of a lookup: O(1)
+    [[nodiscard]] constexpr E to_enum(std::string_view value) const {
+        uint32_t h = hash(value) % HASH_TABLE_SIZE;
+        while (hash_table[h].first != 0) {
+            if (hash_table[h].first == hash(value)) {
+                return hash_table[h].second;
+            }
+            h = (h + 1) % HASH_TABLE_SIZE;
+        }
+        throw std::out_of_range("String value not found in the mapping");
+    }
+
+    // Case insensitive to_enum function but average time complexity is O(n)
+    [[nodiscard]] constexpr E to_enum_insensitive(std::string_view value) const {
         auto it = std::ranges::find_if(mappings, [value](const auto& pair) {
-            return pair.string_val == value; });
+            return case_insensitive_equal(pair.string_val, value);
+        });
 
         if (it == mappings.end())
             throw std::out_of_range("String value not found in the mapping");
 
         return it->enum_val;
-    }
-
-    [[nodiscard]] constexpr std::vector<E> to_enum_all(std::string_view value) const {
-        std::vector<E> res;
-        for_each_pair([&res, value](E enum_value, std::string_view str_value) {
-            if (str_value == value) {
-                res.push_back(enum_value);
-            }
-        });
-        return res;
     }
 
     [[nodiscard]] constexpr std::string_view to_string(E value) const {
@@ -56,6 +79,22 @@ public:
             throw std::out_of_range("Enum value not found in the mapping");
 
         return it->string_val;
+    }
+
+    [[nodiscard]] constexpr std::array<E, N> get_enum_all() const {
+        std::array<E, N> res;
+        for (std::size_t i = 0; i < N; i++) {
+            res[i] = mappings[i].enum_val;
+        }
+        return res;
+    }
+
+    [[nodiscard]] constexpr std::array<std::string_view, N> get_string_all() const {
+        std::array<E, N> res;
+        for (std::size_t i = 0; i < N; i++) {
+            res[i] = mappings[i].string_val;
+        }
+        return res;
     }
 
     constexpr bool contains(E target) const {
@@ -114,6 +153,19 @@ private:
     };
 
     std::array<EnumStringPair, N> mappings;
+
+    static constexpr std::size_t HASH_TABLE_SIZE = N * 2;
+    std::array<std::pair<uint32_t, E>, HASH_TABLE_SIZE> hash_table{};
+
+    constexpr void build_hash_table() {
+        for (const auto& pair : mappings) {
+            uint32_t h = hash(pair.string_val) % HASH_TABLE_SIZE;
+            while (hash_table[h].first != 0) {
+                h = (h + 1) % HASH_TABLE_SIZE;
+            }
+            hash_table[h] = {hash(pair.string_val), pair.enum_val};
+        }
+    }
 }; // class EnumString
 
 template<EnumType E, std::size_t N>
